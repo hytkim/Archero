09642123f4fb9ebb29525cea8b718be8d27c216b
SGB1801082 <145666445+hytkim@users.noreply.github.com>
2024-07-14 00:47:13 +0900

    레이저 충돌 이펙트 수정

--- Assets/Script/Enemy/LaserCat.cs
+++ Assets/Script/Enemy/LaserCat.cs
@@ -20,14 +20,23 @@ public class LaserCat : MonoBehaviour
         {
             Physics.Raycast(transform.position, transform.forward, out RaycastHit hit);
 
-            if (hit.transform.CompareTag("Player") || hit.transform.CompareTag("Wall"))
+            if (hit.transform.CompareTag("Player") || hit.transform.CompareTag("Wall"))//버그왜나는거야 진짜..
             {
                 HitEffect.SetActive(true);
                 HitEffect.transform.position = hit.point;
                 if (hit.transform.CompareTag("Player"))
                 {
                     Debug.Log("Lager Atk Run");
+
+                    if (Laser.activeSelf == false)
+                    {
+                        return;
+                    }
                     PlayerHpBar.Single.curHp -= Time.deltaTime * 250f;// LaserDMG;
+                }
+                else
+                {
+                    Debug.Log("Player is Escape");
                 }
             }
             else
